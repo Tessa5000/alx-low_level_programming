f28b81fe9aad124ed198b9017ac3a784d7fd3c54
Tessa5000 <vennytess@gmail.com>
2023-06-19 18:33:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
+ * main-Entry point
  *
- * Return: Always 0 (success)
- * /
- {
-	puts("\"programming is like building a multilingual puzzle");
-	rturn (0);
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"programming is liking building a multilingual puzzle");
+	return (0);
 }
